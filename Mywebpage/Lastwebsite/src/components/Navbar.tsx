63061e0191c157bc6b811ff94b5323d395598f74
Hicham1970 <devhicham2@gmail.com>
2025-11-30 19:20:29 +0100

    tweaking the dark mode as default

--- Mywebpage/Lastwebsite/src/components/Navbar.tsx
+++ Mywebpage/Lastwebsite/src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
-  const [isDark, setIsDark] = useState(false);
+  const [isDark, setIsDark] = useState(true);
 
   useEffect(() => {
     if (isDark) {
